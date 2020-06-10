14929bf1daed810488658798ed45f13c74c4ac5d
Difinition <vladdifinition@mail.ru>
2020-06-10 18:53:49 +0300

    Обновление темной темы

--- src/js/basket.js
+++ src/js/basket.js
@@ -15,3 +15,18 @@ function stateBasket(state) {
   basket.setAttribute('data-state', `${state === 'close' ? 'open' : 'close'}`);
   state === 'close' ? ScrollControl.lock() : ScrollControl.unlock();
 }
+
+
+
+// Переключение черной темы
+let selectorTheme = document.querySelector('.selector__theme');
+let container = document.querySelector('[data-container]');
+
+if (selectorTheme) selectorTheme.addEventListener('click', stateTheme);
+
+function stateTheme() {
+  this.classList.toggle('selector__active');
+
+  document.body.classList.toggle('container--black');
+  container.classList.toggle('container--black');
+}
